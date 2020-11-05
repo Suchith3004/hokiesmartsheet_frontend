cd4f7aee6d82fc314a14e8c22c77494bbfba166f
Metasyn <kdkelley@vt.edu>
2020-11-04 21:16:21 -0500

    fixed all text turning orange when a class is being dragged

--- checksheet/Class.js
+++ checksheet/Class.js
@@ -29,6 +29,7 @@ export default class Task extends React.Component{
                         {...provided.draggableProps}
                         ref={provided.innerRef}
                         isDragging = {snapshot.isDragging}
+                        className="classHandleText"
                     >
                         <Handle  {...provided.dragHandleProps} />
                         {this.props.task.id}
