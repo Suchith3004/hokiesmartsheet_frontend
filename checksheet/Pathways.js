f30f943f15814482774ff40bad1c58d53e7e9952
Suchith Suddala <Suchiths@vt.edu>
2020-11-30 21:27:17 -0500

    merge inpageNav and mentorSearch

---
+++ checksheet/Pathways.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import NavBar from '../utilities/NavBar'
+
+class Pathways extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Pathways Page Temp</h1>
+            </div>
+        );
+    }
+
+}
+
+export default Pathways;
